fb97ecc85d4c9caabac9ba075370a80b8bad7809
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-01 18:27:18 +0100

    create mutation to add a user to db

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -7,7 +7,16 @@ module.exports = gql`
         username: String!,
         createdAt: String!
     }
+    input RegisterInput {
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
     type Query {
         getPosts: [Post]
+    }
+    type Mutation {
+        register(registerInput: RegisterInput)
     }
 `
