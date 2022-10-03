8789929bb636fe682ad24a20b973614cac303659
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-04 00:17:41 +0100

    Revert "create type definitions"
    This reverts commit 7f4e6d078f92e45ba84cf91c2100e18833fe33ee.

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -22,12 +22,9 @@ module.exports = gql`
     }
     type Query {
         getPosts: [Post]
-        getPost(postId: ID!): Post!
     }
     type Mutation {
         register(registerInput: RegisterInput): User!
-        login(username: String!, password: String!): User!
-        createPost(body: String!): Post!
-        deletePost(postId: ID!): String!
+        login(ysername: String!, password: String!): User!
     }
 `
