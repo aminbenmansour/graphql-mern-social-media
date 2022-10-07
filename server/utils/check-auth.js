1a0e557612f0121d31754437f06b2b425edf6d45
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-07 20:49:28 +0100

    updating they way we acquire bearer token

--- server/utils/check-auth.js
+++ server/utils/check-auth.js
@@ -1,24 +1,20 @@
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError } = require("apollo-server");
 
-const jwt = require("jsonwebtoken")
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const SECRET_KEY = process.env.SECRET_KEY
+const SECRET_KEY = process.env.SECRET_KEY;
 
 module.exports = (context) => {
-    const authHeader = context.req.headers.authorization
-    if(authHeader) {
-        const token = authHeader.split("Bearer ")[1]
-        if(token) {
-            try {
-                const user = jwt.verify(token, SECRET_KEY)
-                return user
-            } catch (error) {
-                throw new AuthenticationError("Invalid/Expired token")
-            }
-            
-        }
-        throw new Error("Authentication token must be \'Bearer [token]\'")
+  const token = context.req.headers.bearer;
+  if (token) {
+    try {
+      const user = jwt.verify(token, SECRET_KEY);
+      return user;
+    } catch (error) {
+      throw new AuthenticationError("Invalid/Expired token");
     }
-    throw new Error("Authentication header must be provided")
-}
+
+  }
+  throw new Error("Authorization header must be provided");
+};
