3543986bbf8f2a0e0583c00b6a7423f03e045e71
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-07 21:53:52 +0100

    imports and embeds for implementing a subscription with apollo server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer, PubSub } = require('apollo-server')
 const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs')
@@ -7,10 +7,12 @@ const resolvers = require('./graphql/resolvers')
 require('dotenv').config()
 const MONGO_URL = process.env.MONGO_URL
 
+const pubsub = new PubSub()
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req, pubsub })
 });
 
 mongoose
