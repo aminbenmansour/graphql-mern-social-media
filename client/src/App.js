ad71de72a7b7197e8310025a6c1561bb21fbea1f
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-11 23:30:42 +0100

    rendering a menubar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import "semantic-ui-css/semantic.min.css";
 import './App.css';
+
+import MenuBar from "./Components/MenuBar";
 
 import Home from "./Pages/Home"
 import Login from "./Pages/Login"
@@ -10,6 +12,7 @@ import Register from "./Pages/Register"
 function App() {
   return (
     <Router>
+      <MenuBar />
       <Routes>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
