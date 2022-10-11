7e47d32e459d478c86445c5af06684c38402444c
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-11 23:42:24 +0100

    container for wrapping content

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";
-import './App.css';
+import "./App.css";
 
 import MenuBar from "./Components/MenuBar";
 
-import Home from "./Pages/Home"
-import Login from "./Pages/Login"
-import Register from "./Pages/Register"
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
     <Router>
-      <MenuBar />
-      <Routes>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Routes>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
