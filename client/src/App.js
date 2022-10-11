b27873073679019d6c0b5bf1d10aa7a1e3d75c89
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-12 00:00:02 +0100

    fix import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";
 import './App.css';
