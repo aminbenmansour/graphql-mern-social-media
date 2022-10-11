d9c999e0017da2075b3fdd8846e4ad57d6331b10
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-11 23:11:51 +0100

    wrapping each route in routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Routes>
     </Router>
   );
 }
