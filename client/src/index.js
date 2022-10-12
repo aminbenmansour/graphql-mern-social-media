ed3dc1a6b889718240a0649eca243e7461f463a0
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-12 16:54:49 +0100

    recommended way to wrap app with apollo provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import App from "./App"
 import "./index.css";
-import ApolloProvider from "./Providers/ApolloProvider";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {ApolloProvider}
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
 );
