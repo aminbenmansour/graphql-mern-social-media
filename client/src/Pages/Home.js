94a94f79d2d95df6b5d5cde95f765d0f1762ac93
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-12 17:08:59 +0100

    send a query to apollo server

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,36 @@
+import { gql, useQuery } from "@apollo/react-hooks";
+
 const Home = () => {
-    return ( 
-        <div>
-            <h1>Home Page</h1>
-        </div>
-     );
-}
- 
+  const { loading, data } = useQuery(FETCH_POSTS);
+  console.log(data);
+  console.log(loading);
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+const FETCH_POSTS = gql`
+  query getPosts {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        body
+        createdAt
+      }
+    }
+  }
+`;
+
 export default Home;
