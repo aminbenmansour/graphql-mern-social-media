91952e8528deffecc500bf43de1b47a4b7c99ead
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-13 23:59:10 +0100

    template for rendering posts

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,12 +1,30 @@
 import { gql, useQuery } from "@apollo/react-hooks";
 
+import { Grid, Image } from "semantic-ui-react";
 const Home = () => {
   const { loading, data } = useQuery(FETCH_POSTS);
-  console.log(data);
-  console.log(loading);
+
+  const posts = data.getPosts;
   return (
     <div>
       <h1>Home Page</h1>
+
+      <Grid columns={3} divided>
+        <Grid.Row>
+          <h1>Recent posts</h1>
+        </Grid.Row>
+
+        <Grid.Row>
+          {loading ? (
+            <h1> Loading posts</h1>
+          ) : (
+            posts &&
+            posts.map((el) => {
+              <Grid.Column></Grid.Column>;
+            })
+          )}
+        </Grid.Row>
+      </Grid>
     </div>
   );
 };
