46fd9ea22f15d429e346c394ec4358b8ef4ab2b5
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-14 01:36:35 +0100

    hyperlink to post on timestamp

--- client/src/Components/PostCard.jsx
+++ client/src/Components/PostCard.jsx
@@ -1,11 +1,11 @@
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, Icon, Label, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-import moment from "moment"
+import moment from "moment";
 
 const PostCard = (props) => {
-  const { body, username, createdAt } = props.post;
+  const { id, body, username, createdAt, likeCount } = props.post;
 
-  console.log(props)
   return (
     <Card>
       <Card.Content>
@@ -15,14 +15,14 @@ const PostCard = (props) => {
           src="https://react.semantic-ui.com/images/avatar/large/molly.png"
         />
         <Card.Header>{username}</Card.Header>
-        <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
-        <Card.Description>
-          {body}
-        </Card.Description>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow()}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className="ui two buttons">
-          <p>Buttons here</p>
+          
         </div>
       </Card.Content>
     </Card>
