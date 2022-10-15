61b15593fbe6c379e30e1ccd9c912b357ea1916c
Amine BEN MANSOUR <50111205+aminbenmansour@users.noreply.github.com>
2022-10-15 22:27:53 +0100

    implementing login page (#17)
    * getting rid of console logs
    
    * implementing login page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,9 +1,93 @@
+import { useState } from "react";
+import { gql } from "@apollo/client";
+import { useMutation } from "@apollo/react-hooks";
+import { useNavigate } from "react-router-dom";
+import { Button, Form } from "semantic-ui-react";
+
+import { useForm } from "../Hooks/hooks";
+
 const Login = () => {
-    return ( 
-        <div>
-            <h1>Login Page</h1>
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState({});
+
+  const initialState = {
+    username: "",
+    password: ""
+  };
+
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, initialState);
+  
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update() {
+      navigate("/");
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.errors);
+    },
+    variables: values,
+  });
+
+  function loginUserCallback() {
+    loginUser();
+  }
+  return (
+    <div className="form-container">
+      <h1>Login Page</h1>
+
+      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
+        <Form.Input
+          label="Username"
+          name="username"
+          type="text"
+          placeholder="Username"
+          value={values.username}
+          error={errors.username ? true : false}
+          onChange={onChange}
+        />
+
+        <Form.Input
+          label="Password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={values.password}
+          error={errors.password ? true : false}
+          onChange={onChange}
+        />
+
+    
+        <Button type="submit" primary>
+         Login
+        </Button>
+      </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
         </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
+const LOGIN_USER = gql`
+  mutation login(
+    $username: String!
+    $password: String!
+  ) {
+    login(
+      username: $username
+      password: $password
+    ) {
+      username
+      email
+      token
+    }
+  }
+`;
+
 export default Login;
