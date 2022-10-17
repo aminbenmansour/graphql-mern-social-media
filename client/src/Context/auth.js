7cb6d3694c8aade688138012edaf03bef49824ba
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-17 23:36:23 +0100

    creating a context and reducer

---
+++ client/src/Context/auth.js
@@ -0,0 +1,24 @@
+import { createContext } from "react";
+
+const AuthContext = createContext({
+  user: null,
+  login: (data) => {},
+  logout: () => {},
+});
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+}
