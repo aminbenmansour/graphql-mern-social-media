5a0753f68059fb5a4a13205d39c0b0d30cff6673
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-25 23:52:46 +0100

    creating form structure

--- client/src/Components/PostForm.jsx
+++ client/src/Components/PostForm.jsx
@@ -1,5 +1,21 @@
+import { Form, Button } from "semantic-ui-react";
+
 const PostForm = () => {
-  return <div></div>;
+  return (
+    <Form onSubmit={onSubmit}>
+      <h2>Create a post:</h2>
+      <Form.Field>
+        <Form.Input
+          placeholder="Hi World!"
+          onChange={onChange}
+          value={values.body}
+        />
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
+      </Form.Field>
+    </Form>
+  );
 };
 
 export default PostForm;
