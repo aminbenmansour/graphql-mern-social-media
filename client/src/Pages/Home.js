64fcb557af07d152a698f9db1a8bd4bc22d95f42
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-27 23:26:07 +0100

    rendering postform component

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { gql, useQuery } from "@apollo/react-hooks";
 import { Grid } from "semantic-ui-react";
 
+import { AuthContext } from "../Context/auth";
+
 import PostCard from "../Components/PostCard";
+import PostForm from "../Components/PostForm";
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
   const { loading, data } = useQuery(FETCH_POSTS);
 
   return (
@@ -12,14 +17,19 @@ const Home = () => {
 
       <Grid columns={3} fluid>
         <Grid.Row>
+          {user && (
+            <Grid.Column>
+              <PostForm />
+            </Grid.Column>
+          )}
           {loading ? (
             <h1> Loading posts</h1>
           ) : (
             data.getPosts &&
             data.getPosts.map((post) => {
               return (
-                <Grid.Column key={post.id} style={{marginBotton: 20}}>
-                  <PostCard post={post} style={{marginButton: 20}} />
+                <Grid.Column key={post.id} style={{ marginBotton: 20 }}>
+                  <PostCard post={post} style={{ marginButton: 20 }} />
                 </Grid.Column>
               );
             })
