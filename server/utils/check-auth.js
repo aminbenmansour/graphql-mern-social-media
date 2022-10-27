0a9bce4874ece21b5ff9c2c49c1b758195d8351e
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-10-28 00:33:32 +0100

    revert last changes (i was tired and tested uncorrectly)

--- server/utils/check-auth.js
+++ server/utils/check-auth.js
@@ -6,15 +6,18 @@ require("dotenv").config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 module.exports = (context) => {
-  const token = context.req.headers.bearer;
-  if (token) {
-    try {
-      const user = jwt.verify(token, SECRET_KEY);
-      return user;
-    } catch (error) {
-      throw new AuthenticationError("Invalid/Expired token");
+  const authHeader = context.req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split("Bearer ")[1];
+    if (token) {
+      try {
+        const user = jwt.verify(token, SECRET_KEY);
+        return user;
+      } catch (error) {
+        throw new AuthenticationError("Invalid/Expired token");
+      }
     }
-
+    throw new Error("Authentication token must be 'Bearer [token]'");
   }
   throw new Error("Authorization header must be provided");
 };
